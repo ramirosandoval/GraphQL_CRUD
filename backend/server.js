5ef787b9e367606779d641eed4b88ad8b6a735cb
ramirosandoval <ramiro_sando@outlook.com>
2022-06-11 02:43:47 -0300

    dependencies updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 (async function startApolloServer(){
     await apolloServer.start()
     apolloServer.applyMiddleware({ app });
-})()
+})();
 
 app.listen({ port : 4000}, () =>{
     console.log("http://localhost:4000" + apolloServer.graphqlPath);
